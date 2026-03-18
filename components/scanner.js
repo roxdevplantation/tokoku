43bd63e9a4ee60455fd31c1238f2f2e3b9588429
rockysam <roxdevplantation@gmail.com>
2026-03-18 23:19:44 +0700

    modul terfocus

--- components/scanner.js
+++ components/scanner.js
@@ -244,15 +244,16 @@ function _start() {
           return { width: w, height: h };
         },
         aspectRatio: 1.5,
+        // -----hilangkan komentar untuk  banyak mendukung scanner
         formatsToSupport: [
           Html5QrcodeSupportedFormats.EAN_13,
-          Html5QrcodeSupportedFormats.EAN_8,
-          Html5QrcodeSupportedFormats.CODE_128,
-          Html5QrcodeSupportedFormats.CODE_39,
-          Html5QrcodeSupportedFormats.CODE_93,
-          Html5QrcodeSupportedFormats.UPC_A,
-          Html5QrcodeSupportedFormats.UPC_E,
-          Html5QrcodeSupportedFormats.ITF,
+          // Html5QrcodeSupportedFormats.EAN_8,
+          // Html5QrcodeSupportedFormats.CODE_128,
+          // Html5QrcodeSupportedFormats.CODE_39,
+          // Html5QrcodeSupportedFormats.CODE_93,
+          // Html5QrcodeSupportedFormats.UPC_A,
+          // Html5QrcodeSupportedFormats.UPC_E,
+          // Html5QrcodeSupportedFormats.ITF,
         ],
       },
       (code) => _onResult(code),
